bf9e0e6ff416fff7f5488d84b032eb1869df93f5
Wijnand Karsens <wijnand@karsens.com>
2024-04-18 18:17:11 +0200

    improvements

--- src/api-util.ts
+++ src/api-util.ts
@@ -24,7 +24,7 @@ export const getObjectKeysArray = <TObject extends { [key: string]: any }>(
 };
 
 /** Use this to create an endpoint based on an openapi definition */
-export const makeEndpoint = (fn: Endpoint<any>) => {
+export const makeEndpoint = (fn: Endpoint<any>, isFlyFn?: boolean) => {
   return async function handler(req: NextApiRequest, res: NextApiResponse) {
     const operations = openapi?.paths
       ? Object.values(openapi.paths)
@@ -70,6 +70,24 @@ export const makeEndpoint = (fn: Endpoint<any>) => {
           errors.map((x) => x.instancePath + ": " + x.message).join(" \n\n"),
         // errors,
       });
+      return;
+    }
+
+    // For fly-required functions in vercel env, we want to fetch fly.dev
+    if (isFlyFn && !process.env.IS_RUNNER) {
+      // TODO: Get this by using the Fly API
+      const FLY_DOMAIN = "bun-wijnand-falling-cloud-2567.fly.dev";
+      const flyUrl = `https://${FLY_DOMAIN}/api/${fn.name}`;
+
+      // TODO: should respond with something within 25s (see https://vercel.com/docs/functions/runtimes#max-duration) and finally respond with the response from the runner.
+      const result = await fetch(flyUrl, { method: "POST", body: data }).then(
+        (res) => res.json(),
+      );
+
+      //
+
+      /** Use this to create an endpoint that spawns or finds a worker from a pool that can execute more long-running code that cannot be executed in Vercel */
+
       return;
     }
     const result = await fn(req.body);
